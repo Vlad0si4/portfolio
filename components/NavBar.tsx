24b142d4ae384237a02f427861f7346ed4a3ac45
Vlad0si4 <boiko.vlad91@gmail.com>
2024-03-08 12:28:59 +0200

    added: new project

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <div className="fixed top-0 p-4 bg-transparent z-[20] w-full flex items-center flex-col sm:flex-row gap-5 sm:justify-between sm:px-14">
-      <h1 className="text-white text-4xl md:text-6xl">
-        Vladislav<span className="font-thin"> Boiko</span>
-        <span className="text-red-500">.</span>
-      </h1>
+      <Link href="/">
+        <h1 className="text-white text-4xl md:text-6xl cursor-pointer">
+          Vladislav<span className="font-thin"> Boiko</span>
+          <span className="text-red-500">.</span>
+        </h1>
+      </Link>
 
       <div className="flex flex-row gap-5 justify-center">
         {Socials.map((social) => (
